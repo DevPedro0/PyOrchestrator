3c57d458bfd86bef90e2050923ab2b7456ec74a3
Pedro Henrique Marques <developerpedro0@gmail.com>
2026-02-18 16:31:48 -0300

    Implement Register File and Confirm Campus CPF

--- project/register/register.py
+++ project/register/register.py
@@ -1,8 +1,11 @@
 import re
+import hashlib
+import uuid
+import base64
 
 class Register():
+    confirm_register = False
     def __init__(self, cpf, name, surname, email, password, confirm_password, *args, **kwargs):
-        self.url = kwargs.get("url", "")
         self.cpf = cpf
         self.name = name
         self.surname = surname
@@ -10,6 +13,50 @@ class Register():
         self.password = password
         self.c_password = confirm_password
         self.a = args
+        self.status_user = {}
     
-    def register(self):
-        ...
+    # Campus 
+    def CPF(self):
+        if len(self.cpf) != 9:
+            raise Exception(f"The CPF value does not match reality; it has {len(self.cpf)} digits.")
+        
+        while True:
+            n = self.cpf.__len__()
+            start = 1 if n == 9 else 0
+            char_save = 0
+            if n != 11:
+                for Index, i in enumerate(range(start, n+start)):
+                    char = int(self.cpf[Index]) * i
+                    char_save += char
+                    if (Index+1 == n):
+                        calc = char_save % 11
+                        self.cpf += str(calc)
+            else:
+                return self.cpf
+            
+    def EMAIL(self):
+        return self.email
+        
+    def PASSWORD(self):
+        key = 0
+        return key
+    
+    @property
+    def _NAME(self):
+        f"{self.name} - {self.surname}"
+    
+    @property
+    def _CPF(self):
+        return self.CPF()
+    
+    @property
+    def _EMAIL(self):
+        return self.EMAIL()
+    
+    def __getattr__(self, item):
+        if (not self.confirm_register):
+            # Block User or Redirect User for Page Register
+            ...
+    
+R = Register("152627416", "Pedro", "Henrique", "pedroexemplo@gmail.com", 1234, 1234)
+R.CPF()
